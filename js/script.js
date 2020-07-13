abdc7a5c032510239832459411385a00713314a7
yuridido <yuridido@yahoo.it>
2020-07-13 17:20:19 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ var inserimento = prompt('inserisci il tuo cognome');
 var cognome = inserimento.charAt(0).toUpperCase() + inserimento.slice(1).toLowerCase();
 var elenco ='';
 var i = 0;
+var corrispondenza = false;
 
 //inserisco il cognome nell'array e ordino alfabeticamente
 listaCognomi.push(cognome);
@@ -21,3 +22,16 @@ do {
     i++;
 } while(i < listaCognomi.length);
 document.getElementById('elenco-cognomi').innerHTML = elenco;
+
+// stampo con while
+for (var j=0; j < listaCognomi.length; j++) {
+    if (listaCognomi[j] == cognome) {
+        var numerazione = ++j;
+        j = listaCognomi.length;
+    }
+}
+document.getElementById('posizione').innerHTML = "la posizione del tuo cognome è: "+ numerazione;
+
+// stampo con l'indexOf
+// var a = listaCognomi.indexOf(cognome) + 1;
+// document.getElementById('posizione').innerHTML = "la posizione del tuo cognome è: "+ a;
